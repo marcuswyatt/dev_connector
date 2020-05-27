72027bfddf6239e4fa8fb6fa076a38d78f54aa88
Marcus Wyatt <marcus@exceptionz.com>
2020-05-27 14:31:11 +1200

    42. User Login

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,4 +1,4 @@
-import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR } from '../actions/types';
+import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL } from '../actions/types';
 
 const initialState = {
   token: localStorage.getItem('token'),
@@ -13,10 +13,12 @@ export default function (state = initialState, action) {
     case USER_LOADED:
       return {
         ...state,
+        isAuthenticated: true,
         loading: false,
         user: payload,
       };
     case REGISTER_SUCCESS:
+    case LOGIN_SUCCESS:
       localStorage.setItem('token', payload.token);
       return {
         ...state,
@@ -26,6 +28,7 @@ export default function (state = initialState, action) {
       };
     case AUTH_ERROR:
     case REGISTER_FAIL:
+    case LOGIN_FAIL:
       localStorage.removeItem('token');
       return {
         ...state,
