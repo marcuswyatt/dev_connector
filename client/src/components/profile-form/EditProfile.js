00a205d298da962954657336c3c9325bb6f16e39
Marcus Wyatt <marcus@exceptionz.com>
2020-05-29 11:03:55 +1200

    71. Fix EditProfile issue

--- client/src/components/profile-form/EditProfile.js
+++ client/src/components/profile-form/EditProfile.js
@@ -26,20 +26,21 @@ const EditProfile = ({ profile: { profile, loading }, createProfile, history, ge
     getCurrentProfile();
 
     setFormData({
-      company: loading || !profile.company ? '' : profile.company,
-      website: loading || !profile.website ? '' : profile.website,
-      location: loading || !profile.location ? '' : profile.location,
-      status: loading || !profile.status ? '' : profile.status,
-      skills: loading || !profile.skills ? '' : profile.skills.join(','),
-      githubusername: loading || !profile.githubusername ? '' : profile.githubusername,
-      bio: loading || !profile.bio ? '' : profile.bio,
-      twitter: loading || !profile.social ? '' : profile.social.twitter,
-      facebook: loading || !profile.social ? '' : profile.social.facebook,
-      linkedin: loading || !profile.social ? '' : profile.social.linkedin,
-      youtube: loading || !profile.social ? '' : profile.social.youtube,
-      instagram: loading || !profile.social ? '' : profile.social.instagram,
+      company: loading || !profile || !profile.company ? '' : profile.company,
+      website: loading || !profile || !profile.website ? '' : profile.website,
+      location: loading || !profile || !profile.location ? '' : profile.location,
+      status: loading || !profile || !profile.status ? '' : profile.status,
+      skills: loading || !profile || !profile.skills ? '' : profile.skills.join(','),
+      githubusername: loading || !profile || !profile.githubusername ? '' : profile.githubusername,
+      bio: loading || !profile || !profile.bio ? '' : profile.bio,
+      twitter: loading || !profile || !profile.social ? '' : profile.social.twitter,
+      facebook: loading || !profile || !profile.social ? '' : profile.social.facebook,
+      linkedin: loading || !profile || !profile.social ? '' : profile.social.linkedin,
+      youtube: loading || !profile || !profile.social ? '' : profile.social.youtube,
+      instagram: loading || !profile || !profile.social ? '' : profile.social.instagram,
     });
-  }, [loading, profile, getCurrentProfile]);
+    // eslint-disable-next-line
+  }, []);
 
   const {
     company,
