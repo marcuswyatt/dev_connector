edd12ee98a730ada13af737bc0b73145ba428322
Marcus Wyatt <marcus@exceptionz.com>
2020-05-28 12:57:03 +1200

    54. Display Profiles

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import PrivateRoute from './components/routing/PrivateRoute';
 import EditProfile from './components/profile-form/EditProfile';
 import AddExperience from './components/profile-form/AddExperience';
 import AddEducation from './components/profile-form/AddEducation';
+import Profiles from './components/profiles/Profiles';
 import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
 
@@ -39,6 +40,7 @@ const App = () => {
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              <Route exact path='/profiles' component={Profiles} />
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
               <PrivateRoute exact path='/create-profile' component={CreateProfile} />
               <PrivateRoute exact path='/edit-profile' component={EditProfile} />
